e1433a73031e6793b06756692cee3526942749ad
Victor Campos Silva <victor.campos@ext.mercadobitcoin.com.br>
2022-03-04 11:57:45 -0300

    test : added report

--- cypress/support/index.js
+++ cypress/support/index.js
@@ -1,1 +1,11 @@
 import "./commands";
+import addContext from 'mochawesome/addContext'
+
+Cypress.on("test:after:run", (test, runnable) => {
+    
+    let videoName = Cypress.spec.name
+    videoName = videoName.replace('/.js.*', '.js')
+    const videoUrl = 'videos/' + videoName + '.mp4'
+
+    addContext({ test }, videoUrl)
+});
